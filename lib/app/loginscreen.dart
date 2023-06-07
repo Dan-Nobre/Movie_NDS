eb5a1f019ab9de0ab5413479befa50fe4b283bee
Daniel Araujo <danielaraujonobre14@gmail.com>
2023-06-07 19:06:24 -0300

    Page change LoginScreen

--- lib/app/loginscreen.dart
+++ lib/app/loginscreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,35 +11,64 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(40, 40, 44, 1),
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(40, 40, 44, 1),
         centerTitle: true,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('imagens/Group 1.png',
-            fit: BoxFit.contain,
-            height: 27,
+            Image.asset(
+              'imagens/Group 1.png',
+              fit: BoxFit.contain,
+              height: 27,
             ),
-            Container(
-              padding: const EdgeInsets.all(82),
-              child: Text("Login")
-            )
+            Container(padding: const EdgeInsets.all(82), child: Text("Login"))
           ],
         ),
       ),
       body: Container(
+        
         child: SingleChildScrollView(
           padding: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 96),
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
+
+              Padding(padding: EdgeInsets.only(bottom: 96)),
+
+              TextField(
+                keyboardType: TextInputType.emailAddress, 
+                decoration: InputDecoration(
+                  hintText: "hola@soytian.tech",
+                  labelText: 'E-mail',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0)
+                  ),
+                
                 ),
-              )
+              ),
+
+              Padding(padding: EdgeInsets.only(bottom: 74)),
+
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0)
+                  )
+                ),
+              ),
+
+              Text("Forgot Your Password?"),
+
+              ElevatedButton(onPressed: () {},
+              child: Text("Continue"),
+              ),
+
+              Text("Don't have an account? Register!")
             ],
           ),
         ),
