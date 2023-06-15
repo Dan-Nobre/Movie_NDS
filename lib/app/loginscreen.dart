87d1d192cab4bc78f45b78d9758f68059676a043
Daniel Araujo <danielaraujonobre14@gmail.com>
2023-06-15 11:52:20 -0300

    Changes in loginScreen, add icon eye

--- lib/app/loginscreen.dart
+++ lib/app/loginscreen.dart
@@ -1,3 +1,5 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
 
 class LoginScreen extends StatefulWidget {
@@ -8,8 +10,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _showPassword = false;
+
   @override
-  
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(40, 40, 44, 1),
@@ -41,59 +44,19 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               const Padding(padding: EdgeInsets.only(bottom: 96)),
 
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "E-mail",
-                  style: TextStyle(
+              Row(
+                children: const [
+                  Padding(padding: EdgeInsets.only(left: 5)),
+                  
+                  Text(
+                    "E-mail",
+                    style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'Poppins',
                       fontWeight: FontWeight.w900),
-                ),
-              ),
-
-              const Padding(padding: EdgeInsets.only(bottom: 5)),
-
-              SizedBox(
-                height: 44,
-                child: TextFormField(
-                decoration: InputDecoration(
-                  filled: true,
-                      fillColor: Colors.white,
-                      hintText: "hola@soytian.tech",
-                      // hintStyle: TextStyle(),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0)
-                      ),
-                  ),
-                ),
-              ),
-
-              const Padding(padding: EdgeInsets.only(bottom: 53)),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Password",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w900),
                     ),
-                  ),
-                  Text(
-                    "Mandatory",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w900),
-                  ),
                 ],
               ),
 
@@ -102,42 +65,76 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 height: 44,
                 child: TextFormField(
-                  obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  // labelText: 'Password',
-                  hintText: 'Password',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                  textAlignVertical: TextAlignVertical.bottom,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "hola@soytian.tech",
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
                   ),
                 ),
-                ),
               ),
-              
 
-              // TextField(
-              //   obscureText: true,
-              //   decoration: InputDecoration(
-              //     filled: true,
-              //     fillColor: Colors.white,
-              //     labelText: 'Password',
-              //     enabledBorder: OutlineInputBorder(
-              //       borderRadius: BorderRadius.circular(8.0),
-              //     ),
-              //   ),
-              // ),
+              const Padding(padding: EdgeInsets.only(bottom: 53)),
+
+              Row(
+                children: const [
+                  Padding(padding: EdgeInsets.only(left: 5)),
+                  
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w900),
+                    ),
+                ],
+              ),
 
               const Padding(padding: EdgeInsets.only(bottom: 5)),
 
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Forgot Your Password?",
-                  style: TextStyle(
-                      color: Color(0xff9F9F9F), fontFamily: "Poppins"),
+              SizedBox(
+                height: 44,
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.bottom,
+                  obscureText: _showPassword == false ? true : false,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    // labelText: 'Password',
+                    hintText: 'Password',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    suffixIcon: GestureDetector(
+                      child: Icon(_showPassword == false
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onTap: () {
+                        setState(() {
+                          _showPassword = !_showPassword;
+                        });
+                      },
+                    ),
+                  ),
                 ),
               ),
+
+              const Padding(padding: EdgeInsets.only(bottom: 5)),
+
+              
+              Row(
+                children: [
+                  const Padding(padding: EdgeInsets.only(left: 5)),
+                  const Text(
+                    "Forgot Your Password?",
+                    style: TextStyle(
+                    color: Color(0xff9F9F9F), fontFamily: "Poppins"),
+                  ),
+                ],
+              ),
+              
 
               const Padding(padding: EdgeInsets.only(bottom: 97)),
 
@@ -169,4 +166,6 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  double get newMethod => sqrt1_2;
 }
